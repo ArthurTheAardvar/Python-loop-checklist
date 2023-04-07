4149affe0f93120f4bf17edff3e86fcb2b030ecc
ArthurTheAardvar <114256031+ArthurTheAardvar@users.noreply.github.com>
2023-04-07 12:59:19 -0600

    Add project files.

---
+++ Python loop checklist/Python_loop_checklist.py
@@ -0,0 +1,15 @@
+for i in range(10):
+    print(i) #print out 0-9
+
+for i in range(10, 20):
+    print(i, end = " ") #print 10-19
+
+for i in range(10, 30, 3):
+    print(i, end= " ")
+
+print()
+
+for i in range(5):
+    for j in range(10):
+        print("*", end = "")
+    print()
